b778da6b20b01ef8890e16e6387f0b8e592085f1
Reknij <jinker2003@gmail.com>
2022-12-26 20:44:20 +0800

    auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import importToCDN from 'vite-plugin-cdn-import'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    importToCDN({
-      modules:[
-        {
-          name: 'vue',
-          var: 'Vue',
-          path: 'https://unpkg.com/vue@3.2.31/dist/vue.runtime.global.prod.js'
-        },
-        {
-          name: 'vue-router',
-          var: 'VueRouter',
-          path: 'https://unpkg.com/vue-router@4.0.14/dist/vue-router.global.prod.js'
-        },
-        {
-          name: 'element-plus',
-          var: 'ElementPlus',
-          path: 'https://unpkg.com/element-plus@2.1.0',
-          css: 'https://unpkg.com/element-plus@2.1.0/dist/index.css'
-        },
-      ]
-    })
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ]
 })
